760825b2850ad9cf920276f0f10724a6a7abce5b
Aris Goudouras <aris.gk@gmail.com>
2025-04-02 12:17:10 +0300

    feat: add diff function stub

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,11 @@
 module Lib
-  ( someFunc,
+  ( diff,
   )
 where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.TreeDiff (Edit)
+import Data.TreeDiff.Tree (EditTree)
+import Text.Pandoc.Definition (Pandoc)
+
+diff :: Pandoc -> Pandoc -> Edit (EditTree a)
+diff = undefined
