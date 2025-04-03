69c32b2d8623a8de97ee9ce2830aea62bf0995fd
Aris Goudouras <aris.gk@gmail.com>
2025-04-03 16:24:50 +0300

    feat: propagate ins and del edit operations for blocks

--- src/Diff.hs
+++ src/Diff.hs
@@ -29,14 +29,26 @@ unfoldAnnotatedTreeFromEditScript = unfoldTree annotatedTreeNodeUnfolder
 annotatedTreeNodeUnfolder :: Edit (EditTree DocNode) -> (DocNode, [Edit (EditTree DocNode)])
 -- Leave Cpy nodes unchanged. Just return their sub-forest edit scripts as the next seeds to be unfolded.
 annotatedTreeNodeUnfolder (Cpy (EditNode docNode subForestEditScripts)) = (docNode, subForestEditScripts)
-annotatedTreeNodeUnfolder (Ins (EditNode (Root) subForestEditScripts)) = undefined
-annotatedTreeNodeUnfolder (Del (EditNode (Root) subForestEditScripts)) = undefined
+annotatedTreeNodeUnfolder (Ins (EditNode (Root) subForestEditScripts)) = (Root, map replaceWithInsOp subForestEditScripts)
+annotatedTreeNodeUnfolder (Del (EditNode (Root) subForestEditScripts)) = (Root, map replaceWithDelOp subForestEditScripts)
 annotatedTreeNodeUnfolder (Swp (EditNode (Root) subForest1EditScripts) (EditNode (Root) subForest2EditScripts)) = undefined
-annotatedTreeNodeUnfolder (Ins (EditNode (TreeNode (BlockNode blockNode)) subForestEditScripts)) = undefined
-annotatedTreeNodeUnfolder (Del (EditNode (TreeNode (BlockNode blockNode)) subForestEditScripts)) = undefined
+annotatedTreeNodeUnfolder (Ins (EditNode (TreeNode (BlockNode blockNode)) subForestEditScripts)) = (TreeNode $ BlockNode blockNode, map replaceWithInsOp subForestEditScripts)
+annotatedTreeNodeUnfolder (Del (EditNode (TreeNode (BlockNode blockNode)) subForestEditScripts)) = (TreeNode $ BlockNode blockNode, map replaceWithDelOp subForestEditScripts)
 annotatedTreeNodeUnfolder (Swp (EditNode (TreeNode (BlockNode blockNode1)) subForest1EditScripts) (EditNode (TreeNode (BlockNode blockNode2)) subForest2EditScripts)) = undefined
 annotatedTreeNodeUnfolder (Ins (EditNode (TreeNode (InlineNode inlineNode)) subForestEditScripts)) = undefined
 annotatedTreeNodeUnfolder (Del (EditNode (TreeNode (InlineNode inlineNode)) subForestEditScripts)) = undefined
 annotatedTreeNodeUnfolder (Swp (EditNode (TreeNode (InlineNode inlineNode1)) subForest1EditScripts) (EditNode (TreeNode (BlockNode inlineNode2)) subForest2EditScripts)) = undefined
--- Here we must return an error because we are in cases where the edit script is wrong (e.g. trying to replace the Root node with another block or inline node).
+-- TODO: Here we must return an error because we are in cases where the edit script is wrong (e.g. trying to replace the Root node with another block or inline node).
 annotatedTreeNodeUnfolder _ = undefined
+
+replaceWithInsOp :: Edit a -> Edit a
+replaceWithInsOp (Cpy node) = Ins node
+replaceWithInsOp (Ins node) = Ins node
+replaceWithInsOp (Del node) = Ins node
+replaceWithInsOp (Swp _ node2) = Ins node2
+
+replaceWithDelOp :: Edit a -> Edit a
+replaceWithDelOp (Cpy node) = Del node
+replaceWithDelOp (Ins node) = Del node
+replaceWithDelOp (Del node) = Del node
+replaceWithDelOp (Swp _ node2) = Del node2
