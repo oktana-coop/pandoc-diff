9833c0381cf8cea2ad48254b69a0f4aeeb6d11ae
Aris Goudouras <aris.gk@gmail.com>
2025-04-03 12:51:35 +0300

    feat: use a flat sequence of text spans for inline content

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,9 +14,15 @@ data BlockNode = PandocBlock Pandoc.Block | ListItem [Pandoc.Block] deriving (Sh
 
 data LinkMark = Link Pandoc.Attr Pandoc.Target deriving (Show, Eq)
 
-data Mark = Emph | Strong | LinkMark LinkMark deriving (Show, Eq)
+data Mark = EmphMark | StrongMark | LinkMark LinkMark deriving (Show, Eq)
 
-data TextSpan = TextSpan T.Text [Mark] deriving (Show, Eq)
+data TextSpan = TextSpan {value :: T.Text, marks :: [Mark]} deriving (Show, Eq)
+
+instance Semigroup TextSpan where
+  (<>) (TextSpan value1 marks1) (TextSpan value2 marks2) = TextSpan (value1 <> value2) (marks1 <> marks2)
+
+instance Monoid TextSpan where
+  mempty = TextSpan T.empty []
 
 data InlineNode = InlineContent [TextSpan] deriving (Show, Eq)
 
@@ -27,8 +33,8 @@ data DocNode = Root | TreeNode TreeNode deriving (Show, Eq)
 traceTree :: Tree DocNode -> Tree DocNode
 traceTree tree = Debug.Trace.trace (drawTree $ fmap show tree) tree
 
-toTree :: Pandoc -> Tree DocNode
-toTree (Pandoc _ blocks) = Node Root $ unfoldForest treeNodeUnfolder $ map (BlockNode . PandocBlock) blocks
+toTree :: Pandoc.Pandoc -> Tree DocNode
+toTree (Pandoc.Pandoc _ blocks) = Node Root $ unfoldForest treeNodeUnfolder $ map (BlockNode . PandocBlock) blocks
 
 treeNodeUnfolder :: TreeNode -> (DocNode, [TreeNode])
 treeNodeUnfolder (BlockNode blockNode) = blockTreeNodeUnfolder blockNode
@@ -46,25 +52,41 @@ blockTreeNodeUnfolder (PandocBlock block) = case block of
 blockTreeNodeUnfolder (ListItem children) = ((TreeNode . BlockNode . ListItem) [], map (BlockNode . PandocBlock) children)
 
 buildInlineNode :: [Pandoc.Inline] -> TreeNode
-buildInlineNode = undefined
+buildInlineNode inlines = InlineNode $ InlineContent $ inlinesToTextSpans inlines
+
+inlinesToTextSpans :: [Pandoc.Inline] -> [TextSpan]
+inlinesToTextSpans = mergeSameMarkSpans . foldMap inlineToTextSpan
+
+mergeSameMarkSpans :: [TextSpan] -> [TextSpan]
+mergeSameMarkSpans = foldr mergeOrAppendAdjacent []
+  where
+    -- This is the folding function for merging the adjacent elements if their marks are the same
+    mergeOrAppendAdjacent :: TextSpan -> [TextSpan] -> [TextSpan]
+    mergeOrAppendAdjacent x [] = [x]
+    -- pattern-match on: the current element (x), the one to its right (firstOfRest) and the rest of the fold
+    mergeOrAppendAdjacent x (firstOfRest : rest) =
+      if marks x == marks firstOfRest
+        -- if the element's marks are the same with the one to its right, we merge them and then add them to the rest of the fold.
+        then (x <> firstOfRest) : rest
+        -- if they are not the same we end up with an extra text span in the list for the current element (we prepend it to the existing list for the fold.)
+        else x : firstOfRest : rest
+
+inlineToTextSpan :: Pandoc.Inline -> [TextSpan]
+inlineToTextSpan inline = case inline of
+  Pandoc.Str str -> [TextSpan str []]
+  Pandoc.Space -> [TextSpan (T.pack " ") []]
+  Pandoc.Strong inlines -> addMark StrongMark inlines
+  Pandoc.Emph inlines -> addMark EmphMark inlines
+  Pandoc.Link attrs inlines target -> addMark (LinkMark $ Lib.Link attrs target) inlines
+  -- TODO: Handle other inline elements
+  _ -> []
+
+addMark :: Mark -> [Pandoc.Inline] -> [TextSpan]
+-- Monoidally add the mark to all text spans created for the inline elements
+addMark mark inlines = fmap (TextSpan T.empty [mark] <>) (inlinesToTextSpans inlines)
 
 inlineTreeNodeUnfolder :: InlineNode -> (DocNode, [TreeNode])
 inlineTreeNodeUnfolder inlineNode = (TreeNode $ InlineNode inlineNode, [])
-
--- inlineTreeNodeUnfolder :: InlineNode -> (DocNode, [TreeNode])
--- inlineTreeNodeUnfolder (PandocInline inline) = case inline of
---   Str txt -> ((TreeNode . InlineNode . PandocInline . Str) txt, [])
---   Emph inlines -> ((TreeNode . InlineNode . PandocInline . Emph) [], map (InlineNode . PandocInline) inlines)
---   Underline inlines -> ((TreeNode . InlineNode . PandocInline . Underline) [], map (InlineNode . PandocInline) inlines)
---   Strong inlines -> ((TreeNode . InlineNode . PandocInline . Strong) [], map (InlineNode . PandocInline) inlines)
---   Strikeout inlines -> ((TreeNode . InlineNode . PandocInline . Strikeout) [], map (InlineNode . PandocInline) inlines)
---   Superscript inlines -> ((TreeNode . InlineNode . PandocInline . Superscript) [], map (InlineNode . PandocInline) inlines)
---   Subscript inlines -> ((TreeNode . InlineNode . PandocInline . Subscript) [], map (InlineNode . PandocInline) inlines)
---   SmallCaps inlines -> ((TreeNode . InlineNode . PandocInline . SmallCaps) [], map (InlineNode . PandocInline) inlines)
---   Space -> ((TreeNode . InlineNode . PandocInline) Space, [])
---   Link attrs inlines target -> (TreeNode $ InlineNode $ PandocInline $ Link attrs [] target, map (InlineNode . PandocInline) inlines)
---   Span attrs inlines -> (TreeNode $ InlineNode $ PandocInline $ Span attrs [], map (InlineNode . PandocInline) inlines)
---   _ -> undefined
 
 diff :: Pandoc -> Pandoc -> Edit (EditTree DocNode)
 diff pandoc1 pandoc2 = treeDiff (traceTree $ toTree pandoc1) (traceTree $ toTree pandoc2)
