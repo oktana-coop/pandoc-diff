5390b45dfca1c93597e6962d615e76c2d1a16d5e
Aris Goudouras <aris.gk@gmail.com>
2025-04-04 18:19:35 +0300

    feat: group same diff and mark inlines (wip)

--- src/RichTextDiff.hs
+++ src/RichTextDiff.hs
@@ -1,20 +1,38 @@
-module Diff
+module RichTextDiff
   ( diff,
     getEditScript,
   )
 where
 
+import Data.Algorithm.Diff as ListDiff (Diff, getDiff)
+import qualified Data.Text as T
 import Data.Tree (Tree, unfoldTree)
 import Data.TreeDiff (Edit (..))
 import Data.TreeDiff.Tree (EditTree (..), treeDiff)
 import DocTree (BlockNode (..), DocNode (..), InlineNode (..), Mark (..), TextSpan (..), TreeNode (..), toTree, traceTree)
 import Text.Pandoc.Definition as Pandoc (Block (Div), Pandoc, nullAttr)
 
+data FormattedCharacter = FormattedCharacter {char :: Char, charMarks :: [Mark]} deriving (Show, Eq)
+
 data MarkDiff = MarkDiff [Mark] [Mark]
 
 data HeadingLevelDiff = HeadingLevelDiff Int Int
 
 data RichTextDiffOp a = Insert a | Delete a | Copy a | UpdateMarks MarkDiff a | UpdateHeadingLevel HeadingLevelDiff a
+
+unpackDiffOpValue :: RichTextDiffOp a -> a
+unpackDiffOpValue (Insert a) = a
+unpackDiffOpValue (Delete a) = a
+unpackDiffOpValue (Copy a) = a
+unpackDiffOpValue (UpdateMarks _ a) = a
+unpackDiffOpValue (UpdateHeadingLevel _ a) = a
+
+instance Functor RichTextDiffOp where
+  fmap f (Insert a) = Insert (f a)
+  fmap f (Delete a) = Delete (f a)
+  fmap f (Copy a) = Copy (f a)
+  fmap f (UpdateMarks markDiff a) = UpdateMarks markDiff (f a)
+  fmap f (UpdateHeadingLevel levelDiff a) = UpdateHeadingLevel levelDiff (f a)
 
 diff :: Pandoc.Pandoc -> Pandoc.Pandoc -> Pandoc.Pandoc
 diff pandoc1 pandoc2 = (toPandoc . unfoldAnnotatedTreeFromEditScript) editScript
@@ -49,15 +67,51 @@ annotatedTreeNodeUnfolder (Ins (EditNode (TreeNode (InlineNode inlineNode)) subF
 annotatedTreeNodeUnfolder (Del (EditNode (TreeNode (InlineNode inlineNode)) subForestEditScripts)) = (Delete $ TreeNode $ InlineNode inlineNode, map replaceWithDelOp subForestEditScripts)
 -- In the case of swapping inlines, we call `handleSwappedInlines` to do the heavy lifting for handling the nodes themselves and create the del+ins operations for the sub-forests
 annotatedTreeNodeUnfolder (Swp (EditNode (TreeNode (InlineNode inlineNode1)) subForest1EditScripts) (EditNode (TreeNode (InlineNode inlineNode2)) subForest2EditScripts)) =
-  (handleSwappedInlines inlineNode1 inlineNode2, handleSwappedSubForests subForest1EditScripts subForest2EditScripts)
+  (diffInlineNodes inlineNode1 inlineNode2, handleSwappedSubForests subForest1EditScripts subForest2EditScripts)
 -- TODO: Here we must return an error because we are in cases where the edit script is wrong (e.g. trying to replace the Root node with another block or inline node).
 annotatedTreeNodeUnfolder _ = undefined
 
 handleSwappedSubForests :: [Edit (EditTree DocNode)] -> [Edit (EditTree DocNode)] -> [Edit (EditTree DocNode)]
 handleSwappedSubForests deletedSubForests insertedSubForests = (map replaceWithDelOp deletedSubForests) <> (map replaceWithInsOp insertedSubForests)
 
-handleSwappedInlines :: InlineNode -> InlineNode -> RichTextDiffOp DocNode
-handleSwappedInlines deletedInlineNode addedInlineNode = undefined
+diffInlineNodes :: InlineNode -> InlineNode -> RichTextDiffOp DocNode
+diffInlineNodes deletedInlineNode addedInlineNode = buildAnnotatedInlineNodeFromDiff $ ListDiff.getDiff (toFormattedText deletedInlineNode) (toFormattedText addedInlineNode)
+
+toFormattedText :: InlineNode -> [FormattedCharacter]
+toFormattedText (InlineContent textSpans) = concatMap textSpanToFormattedText textSpans
+
+textSpanToFormattedText :: TextSpan -> [FormattedCharacter]
+textSpanToFormattedText textSpan = map (\char -> FormattedCharacter char (marks textSpan)) $ T.unpack (value textSpan)
+
+buildAnnotatedInlineNodeFromDiff :: [ListDiff.Diff FormattedCharacter] -> RichTextDiffOp DocNode
+buildAnnotatedInlineNodeFromDiff listDiff = Copy $ TreeNode $ InlineNode $ InlineContent $ groupSameMarkChars $ map listDiffToRichTextDiff listDiff
+
+groupSameMarkAndDiffOpChars :: [RichTextDiffOp FormattedCharacter] -> [RichTextDiffOp TextSpan]
+groupSameMarkAndDiffOpChars = foldr groupOrAppendAdjacent []
+  where
+    -- This is the folding function for grouping the adjacent characters if their marks are the same
+    groupOrAppendAdjacent :: RichTextDiffOp FormattedCharacter -> [RichTextDiffOp TextSpan] -> [RichTextDiffOp TextSpan]
+    -- Keep the diff op the same (just fmap over it) and create a text span with just this character
+    groupOrAppendAdjacent formattedCharWithDiffOp [] = [fmap textSpanFromFormattedChar formattedCharWithDiffOp]
+      where
+        textSpanFromFormattedChar :: FormattedCharacter -> TextSpan
+        textSpanFromFormattedChar (FormattedCharacter c cMarks) = TextSpan (T.pack [c]) cMarks
+    -- pattern-match on: the current element (x), the one to its right (firstOfRest) and the rest of the fold
+    groupOrAppendAdjacent (FormattedCharacter char charMarks) (firstOfRest : rest) =
+      if charMarks == marks firstOfRest
+        -- if the element's marks are the same with the one to its right, we merge them and then add them to the rest of the fold.
+        then (appendCharToTextSpan firstOfRest char) : rest
+        -- if they are not the same we end up with an extra text span in the list for the current element (we prepend it to the existing list for the fold.)
+        else TextSpan (T.pack [char]) charMarks : firstOfRest : rest
+
+    appendCharToTextSpan :: TextSpan -> Char -> TextSpan
+    appendCharToTextSpan textSpan char = TextSpan (appendChar (value textSpan) char) (marks textSpan)
+
+    appendChar :: T.Text -> Char -> T.Text
+    appendChar txt ch = txt `T.snoc` ch
+
+listDiffToRichTextDiff :: ListDiff.Diff FormattedCharacter -> RichTextDiffOp FormattedCharacter
+listDiffToRichTextDiff = undefined
 
 replaceWithInsOp :: Edit a -> Edit a
 replaceWithInsOp (Cpy node) = Ins node
