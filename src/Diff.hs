62b67728b80a4dedcbe7c18d5f9419d0bd9e42de
Aris Goudouras <aris.gk@gmail.com>
2025-04-04 09:38:46 +0300

    feat: handle inline subforests

--- src/Diff.hs
+++ src/Diff.hs
@@ -7,7 +7,7 @@ where
 import Data.Tree (Tree, unfoldTree)
 import Data.TreeDiff (Edit (..))
 import Data.TreeDiff.Tree (EditTree (..), treeDiff)
-import DocTree (BlockNode (..), DocNode (..), TreeNode (..), toPandoc, toTree, traceTree)
+import DocTree (BlockNode (..), DocNode (..), InlineNode (..), TreeNode (..), toPandoc, toTree, traceTree)
 import Text.Pandoc.Definition as Pandoc (Block (Div), Pandoc, nullAttr)
 
 diff :: Pandoc.Pandoc -> Pandoc.Pandoc -> Pandoc.Pandoc
@@ -31,19 +31,24 @@ annotatedTreeNodeUnfolder :: Edit (EditTree DocNode) -> (DocNode, [Edit (EditTre
 annotatedTreeNodeUnfolder (Cpy (EditNode docNode subForestEditScripts)) = (docNode, subForestEditScripts)
 annotatedTreeNodeUnfolder (Ins (EditNode (Root) subForestEditScripts)) = (Root, map replaceWithInsOp subForestEditScripts)
 annotatedTreeNodeUnfolder (Del (EditNode (Root) subForestEditScripts)) = (Root, map replaceWithDelOp subForestEditScripts)
-annotatedTreeNodeUnfolder (Swp (EditNode (Root) subForest1EditScripts) (EditNode (Root) subForest2EditScripts)) = (Root, handleSwappedBlockSubForests subForest1EditScripts subForest2EditScripts)
+annotatedTreeNodeUnfolder (Swp (EditNode (Root) subForest1EditScripts) (EditNode (Root) subForest2EditScripts)) = (Root, handleSwappedSubForests subForest1EditScripts subForest2EditScripts)
 annotatedTreeNodeUnfolder (Ins (EditNode (TreeNode (BlockNode blockNode)) subForestEditScripts)) = (TreeNode $ BlockNode blockNode, map replaceWithInsOp subForestEditScripts)
 annotatedTreeNodeUnfolder (Del (EditNode (TreeNode (BlockNode blockNode)) subForestEditScripts)) = (TreeNode $ BlockNode blockNode, map replaceWithDelOp subForestEditScripts)
 -- In this case of swapping blocks, we add a wrapper div container to the tree and create del+ins operations for the swapped blocks respectively.
 annotatedTreeNodeUnfolder (Swp block1@(EditNode (TreeNode (BlockNode _)) _) block2@(EditNode (TreeNode (BlockNode _)) _)) = (TreeNode $ BlockNode $ PandocBlock $ Pandoc.Div nullAttr [], [Del block1, Ins block2])
 annotatedTreeNodeUnfolder (Ins (EditNode (TreeNode (InlineNode inlineNode)) subForestEditScripts)) = (TreeNode $ InlineNode inlineNode, map replaceWithInsOp subForestEditScripts)
 annotatedTreeNodeUnfolder (Del (EditNode (TreeNode (InlineNode inlineNode)) subForestEditScripts)) = (TreeNode $ InlineNode inlineNode, map replaceWithDelOp subForestEditScripts)
-annotatedTreeNodeUnfolder (Swp (EditNode (TreeNode (InlineNode inlineNode1)) subForest1EditScripts) (EditNode (TreeNode (BlockNode inlineNode2)) subForest2EditScripts)) = undefined
+-- In the case of swapping inlines, we call `handleSwappedInlines` to do the heavy lifting for handling the nodes themselves and create the del+ins operations for the sub-forests
+annotatedTreeNodeUnfolder (Swp (EditNode (TreeNode (InlineNode inlineNode1)) subForest1EditScripts) (EditNode (TreeNode (InlineNode inlineNode2)) subForest2EditScripts)) =
+  (TreeNode $ InlineNode $ handleSwappedInlines inlineNode1 inlineNode2, handleSwappedSubForests subForest1EditScripts subForest2EditScripts)
 -- TODO: Here we must return an error because we are in cases where the edit script is wrong (e.g. trying to replace the Root node with another block or inline node).
 annotatedTreeNodeUnfolder _ = undefined
 
-handleSwappedBlockSubForests :: [Edit (EditTree DocNode)] -> [Edit (EditTree DocNode)] -> [Edit (EditTree DocNode)]
-handleSwappedBlockSubForests deletedSubForests insertedSubForests = (map replaceWithDelOp deletedSubForests) <> (map replaceWithInsOp insertedSubForests)
+handleSwappedSubForests :: [Edit (EditTree DocNode)] -> [Edit (EditTree DocNode)] -> [Edit (EditTree DocNode)]
+handleSwappedSubForests deletedSubForests insertedSubForests = (map replaceWithDelOp deletedSubForests) <> (map replaceWithInsOp insertedSubForests)
+
+handleSwappedInlines :: InlineNode -> InlineNode -> InlineNode
+handleSwappedInlines deletedInlineNode addedInlineNode = undefined
 
 replaceWithInsOp :: Edit a -> Edit a
 replaceWithInsOp (Cpy node) = Ins node
